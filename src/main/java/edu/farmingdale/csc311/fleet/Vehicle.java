8eb445a4d29e080db1d136b1dea563bbaf38327d
Shahla Meem <meemst@farmingdale.edu>
2026-09-03 12:52:53 -0400

    TODO-04: implement honk methods from Honkable

--- src/main/java/edu/farmingdale/csc311/fleet/Vehicle.java
+++ src/main/java/edu/farmingdale/csc311/fleet/Vehicle.java
@@ -178,12 +178,18 @@ public abstract class Vehicle implements Honkable {
 
     @Override
     public void honk() {
-        throw new UnsupportedOperationException("TODO-04");
+        System.out.println(hornSound());
     }
 
     @Override
     public void honk(int times) {
-        throw new UnsupportedOperationException("TODO-04");
+        if (times < 1) {
+            throw new IllegalArgumentException("times: " + times);
+        }
+
+        for (int i = 0; i < times; i++) {
+            System.out.println(hornSound());
+        }
     }
 
     /** Subclasses answer these two. Do not write bodies here. */
